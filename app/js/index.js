b6d016eb0c87f61080729b323ccc517cb9d8916b
DJ Sprouse <daniel.sprouse@nwea.org>
2016-12-21 09:19:24 -0800

    Add color to status input and play sound on submission

--- app/js/index.js
+++ app/js/index.js
@@ -2,35 +2,55 @@ var myApp = angular.module('fortVancouverApp', []);
 
 myApp.controller('MainCtrl', [
     '$scope',
-	'Api',
-    function ($scope, Api) {
-		$scope.showInput = false;
-		$scope.submission = {
-			ticketId: '',
-			status: '',
-			name: ''
-		};
+	'$document',
+    'Api',
+    function ($scope, $document, Api) {
+        $scope.showInput = false;
+        $scope.submission = {
+            ticketId: '',
+            status: '',
+            name: ''
+        };
 
-		$scope.login = function () {
-			$scope.showInput = true;
-		};
+        $scope.login = function () {
+            $scope.showInput = true;
+			//TODO set focus to ticket input
+        };
 
-		$scope.scan = function () {
-			Api.scan($scope.ticketId, $scope.username).then(function (data) {
-				$scope.submission = data;
-			});
-		};
+        $scope.scan = function () {
+            Api.scan($scope.ticketId, $scope.username).then(function (data) {
+                $scope.submission = data;
+				switch($scope.submission.status) {
+					case 'PRIME TICKET':
+						$scope.statusClass = 'prime';
+						$document[0].getElementById('prime-sound').play();
+						break;
+					case 'VALID':
+						$scope.statusClass = 'valid';
+						break;
+					case 'DUPLICATE TICKET ID':
+						$scope.statusClass = 'invalid';
+						$document[0].getElementById('duplicate-sound').play();
+						break;
+					case 'INVALID TICKET ID':
+						$scope.statusClass = 'invalid';
+						$document[0].getElementById('invalid-sound').play();
+						break;
+				}
+				//TODO set focus to ticket input
+            });
+        };
     }
 ]);
 
 myApp.service('Api', [
-	'$http',
-	function ($http) {
+    '$http',
+    function ($http) {
 
-		function scan (ticketId, username) {
-			var url = '../scan/username/' + username + '/ticketId/' + ticketId;
-			
-			return $http.post(url).then(
+        function scan(ticketId, username) {
+            var url = '../scan/username/' + username + '/ticketId/' + ticketId;
+
+            return $http.post(url).then(
                 function (response) {
                     return response.data;
                 },
@@ -38,10 +58,10 @@ myApp.service('Api', [
                     return $q.reject(error);
                 }
             );
-		}
+        }
 
-		return {
-			scan: scan
-		};
-	}
+        return {
+            scan: scan
+        };
+    }
 ]);
