59b8e23777e06fcde558793fc4364407ffcc990c
DJ Sprouse <daniel.sprouse@nwea.org>
2016-12-21 09:33:50 -0800

    Set focus on the appropriate input after submission and on page load

--- app/js/index.js
+++ app/js/index.js
@@ -2,42 +2,52 @@ var myApp = angular.module('fortVancouverApp', []);
 
 myApp.controller('MainCtrl', [
     '$scope',
-	'$document',
+    '$document',
+    '$timeout',
     'Api',
-    function ($scope, $document, Api) {
-        $scope.showInput = false;
+    function ($scope, $document, $timeout, Api) {
+        $scope.showLogin = true;
         $scope.submission = {
             ticketId: '',
             status: '',
             name: ''
         };
 
+        $scope.setFocus = function () {
+            if ($scope.showLogin) {
+                $document[0].getElementById('loginInput').focus();
+            } else {
+                $document[0].getElementById('ticketInput').focus();
+            }
+        };
+
         $scope.login = function () {
-            $scope.showInput = true;
-			//TODO set focus to ticket input
+            $scope.showLogin = false;
+            $timeout($scope.setFocus);
         };
 
         $scope.scan = function () {
             Api.scan($scope.ticketId, $scope.username).then(function (data) {
                 $scope.submission = data;
-				switch($scope.submission.status) {
-					case 'PRIME TICKET':
-						$scope.statusClass = 'prime';
-						$document[0].getElementById('prime-sound').play();
-						break;
-					case 'VALID':
-						$scope.statusClass = 'valid';
-						break;
-					case 'DUPLICATE TICKET ID':
-						$scope.statusClass = 'invalid';
-						$document[0].getElementById('duplicate-sound').play();
-						break;
-					case 'INVALID TICKET ID':
-						$scope.statusClass = 'invalid';
-						$document[0].getElementById('invalid-sound').play();
-						break;
-				}
-				//TODO set focus to ticket input
+                $scope.ticketId = '';
+                $scope.setFocus();
+                switch ($scope.submission.status) {
+                    case 'PRIME TICKET':
+                        $scope.statusClass = 'prime';
+                        $document[0].getElementById('prime-sound').play();
+                        break;
+                    case 'VALID':
+                        $scope.statusClass = 'valid';
+                        break;
+                    case 'DUPLICATE TICKET ID':
+                        $scope.statusClass = 'invalid';
+                        $document[0].getElementById('duplicate-sound').play();
+                        break;
+                    case 'INVALID TICKET ID':
+                        $scope.statusClass = 'invalid';
+                        $document[0].getElementById('invalid-sound').play();
+                        break;
+                }
             });
         };
     }
